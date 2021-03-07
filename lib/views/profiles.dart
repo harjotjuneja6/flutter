295ec0f90f9a5b0eafbc39a52c19511e1fee11ad
Xpake14 <harshkhard@gmail.com>
2021-03-07 19:30:49 +0530

    initial working

--- lib/views/profiles.dart
+++ lib/views/profiles.dart
@@ -1,28 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:vj/views/loginpage.dart';
 import 'package:vj/views/logout.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
 //import 'package:vj/views/detailed.dart';
 
-class Profiles extends StatelessWidget {
+Future<Profile> fetchProfile() async {
+  final response = await http.get(Uri.http('10.0.2.2:4000', 'getGroup'));
+  if (response.statusCode == 200) {
+    return Profile.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Failed to Load");
+  }
+}
+
+class Profile {
+  final String name;
+  final String description;
+  final String phoneNumber;
+  final String genre;
+
+  Profile({this.name, this.description, this.genre, this.phoneNumber});
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+        name: json['name'],
+        description: json['description'],
+        genre: json['genre'],
+        phoneNumber: json['phoneNo']);
+  }
+}
+
+class Profiles extends StatefulWidget {
+  @override
+  _ProfilesState createState() => _ProfilesState();
+}
+
+class _ProfilesState extends State<Profiles> {
+  Future<Profile> futureProfile;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProfile = fetchProfile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: //Container<Widget>(Text('List Of Members'),
-                Container(
-                    alignment: Alignment.topRight,
-                    height: 50,
-                    margin: EdgeInsets.fromLTRB(100, 0, 0, 0),
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: ElevatedButton(
-                      //textColor: Colors.white,
-                      // color: Colors.blue,
-                      child: Text('LogOut'),
-                      onPressed: () => {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LogOutPage()),
-                        )
-                      },
-                    ))));
+      appBar: AppBar(
+        title: Container(
+          alignment: Alignment.bottomRight,
+          child: ElevatedButton(
+            child: Text("Logout"),
+            onPressed: () => {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => LoginPage()))
+            },
+          ),
+        ),
+      ),
+      body: FutureBuilder<Profile>(
+        future: futureProfile,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: <Widget>[
+                Text(snapshot.data.name),
+                Text(snapshot.data.description),
+                Text(snapshot.data.genre),
+                Text(snapshot.data.phoneNumber),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text("Error" + snapshot.error.toString());
+          }
+          return CircularProgressIndicator();
+        },
+      ),
+    );
   }
 }
