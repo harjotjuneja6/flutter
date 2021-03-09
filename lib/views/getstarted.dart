354bf9ea9aafe94c9361e258a2e644abec3c9a8b
Xpake14 <harshkhard@gmail.com>
2021-03-09 17:24:52 +0530

    fixed starting image

--- lib/views/getstarted.dart
+++ lib/views/getstarted.dart
@@ -6,28 +6,23 @@ class Getstarted extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Container(
-            constraints: BoxConstraints.expand(),
+        home: Scaffold(
+            body: Stack(children: <Widget>[
+          Container(
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage("./bgimg.png"), fit: BoxFit.cover)),
-            child: Scaffold(
-                body: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ListView(children: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()),
-                          );
-                        },
-                        child: Text(
-                          'Get Started',
-                        ),
-                      ),
-                      Container()
-                    ])))));
+                    image: AssetImage("assets/images/bgimg.png"),
+                    fit: BoxFit.cover)),
+          ),
+          Center(
+            child: ElevatedButton(
+              child: Text("Get Started"),
+              onPressed: () => {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()))
+              },
+            ),
+          ),
+        ])));
   }
 }
